302496919c4e9ea1ccfd44e5ae4cf039523d3ec3
amandelman <almondman@gmail.com>
2018-10-26 14:26:08 +0200

    Derp

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -13,14 +13,14 @@ function draw() {
   // 	ellipse(x, y, x/10, y/10);
   // 	}
   // }
-
- for(var y = 50; y<=height; y+=50){
- 	for(var x = 50; x<=width; x+=50){
- 		line(x,y,width/2,height/2);
- 		ellipse(x,y,10,10);
- 		
- 	}
- }
+ //
+ // for(var y = 50; y<=height; y+=50){
+ // 	for(var x = 50; x<=width; x+=50){
+ // 		line(x,y,width/2,height/2);
+ // 		ellipse(x,y,10,10);
+ //
+ // 	}
+ // }
 
 
 
@@ -32,7 +32,7 @@ function draw() {
   // ellipse(width/3*2, height/3, 60, 60);
   // ellipse(width/3, height/3*2, 60, 60);
   // ellipse(width/3*2, height/3*2, 60, 60);
-  
+
 
 //Random Drawing
   // quad(800,155, 300,255, 350,355, 850,255);
@@ -42,48 +42,45 @@ function draw() {
 
 
 //Robot Friend
-  // fill(0,0,0);
-  // ellipse(500,200, 100, 100);
 
-  // stroke(0);
-  // line(500, 200, 546, 60);
-  // line(500, 200, 466, 96);
-  // line(500, 200, 567, 130);
+  stroke(0);
+  line(500, 200, 546, 60);
+  line(500, 200, 466, 96);
+  line(500, 200, 567, 130);
 
-  // fill(255, 255, 255);
-  // ellipse(510, 190, 30, 30);
+  fill(255, 255, 255);
+  ellipse(510, 190, 30, 30);
 
-  // fill(0,0,0);
-  // ellipse(510, 190, 5, 5);
+  fill(0,0,0);
+  ellipse(510, 190, 5, 5);
 
-  // fill(160);
-  // ellipse(486, 185, 12, 12);
+  fill(160);
+  ellipse(486, 185, 12, 12);
 
-  // fill(160);
-  // ellipse(523, 170, 10, 10);
+  fill(160);
+  ellipse(523, 170, 10, 10);
 
-  // fill(160);
-  // ellipse(530, 200, 8, 8);
+  fill(160);
+  ellipse(530, 200, 8, 8);
 
-  // stroke(0,0,0);
-  // line(510, 220, 510,320);
-  // line(500, 220, 500,320);
-  // line(490, 220, 490,320);
+  stroke(0,0,0);
+  line(510, 220, 510,320);
+  line(500, 220, 500,320);
+  line(490, 220, 490,320);
 
-  // fill(160);
-  // noStroke();
-  // ellipse(489, 470, 74, 74);
+  fill(160);
+  noStroke();
+  ellipse(489, 470, 74, 74);
 
-  // fill(0);
-  // ellipse(489, 470, 15, 15);
+  fill(0);
+  ellipse(489, 470, 15, 15);
 
-  // fill(0,0,0);
-  // rect(435,320, 105, 150);
-  
-  // fill(160);
-  // noStroke();
-  // rect(435, 340, 105, 10);
+  fill(0,0,0);
+  rect(435,320, 105, 150);
 
-  
+  fill(160);
+  noStroke();
+  rect(435, 340, 105, 10);
+
+
 }
-
